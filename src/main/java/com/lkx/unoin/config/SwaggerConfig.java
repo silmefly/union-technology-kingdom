bbdf1d6791d640634dad9f0024f74a4f4a3ea146
likunxian <Kunxian_Li@human-horizons.com>
2019-09-16 18:38:11 +0800

    add application files and swagger config

--- src/main/java/com/lkx/unoin/config/SwaggerConfig.java
+++ src/main/java/com/lkx/unoin/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.lkx.unoin.config;
 
+import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import springfox.documentation.RequestHandler;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.VendorExtension;
@@ -23,9 +27,23 @@ import java.util.ArrayList;
 public class SwaggerConfig {
 
     @Bean
-    public Docket docket(){
+    public Docket docket(Environment env){
+        //通过enable可以在不同环境中配置是否启用swagger
+        Profiles profiles = Profiles.of(new String[]{"dev","default"});
+        boolean flag = env.acceptsProfiles(profiles);
+
         return new Docket(DocumentationType.SWAGGER_2).
-                apiInfo(apiInfo());
+                apiInfo(apiInfo())
+                //是否启用
+                .enable(flag)
+                .select().apis(input -> {
+                    System.out.println(input);
+                    return true;
+                }).paths(input -> {
+
+                    return true;
+                }).build();
+
     }
 
     /**
@@ -43,10 +61,12 @@ public class SwaggerConfig {
         return new ApiInfo("技术大全接口文档",
                 "包含所有混合技术的接口信息",
                 "V1.0",
-                "urn:tos",//服务条款url
+                //服务条款url
+                "urn:tos",
                  myContact,
                 "Apache 2.0",
                 "http://www.apache.org/licenses/LICENSE-2.0",
+                //服务商扩展参数
                 new ArrayList<VendorExtension>());
 
     }
